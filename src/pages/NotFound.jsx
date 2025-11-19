aabf176fe61bbddf6b714a251a9f05067ad3fe19
Taufiq095 <117509527+Taufiq095@users.noreply.github.com>
2025-11-19 15:30:47 +0800

    tryyy

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ export default function NotFound() {
 
   return (
     <div>
-      <h1>404 - Page Not Found hello</h1>
+      <h1>404 - Page Not Found hello taufiq is here try again tryyyyyy</h1>
       <h1 className="text-8xl">404 - Page Not Found</h1>
     </div>
   )
